69fc45efbaae34c93f30f4932d7b56acab82044d
priskilladrn <priskilla.adriani@gmail.com>
2023-08-24 16:01:20 +0700

    page slide view

--- Tingle Watch App/CrownView.swift
+++ Tingle Watch App/CrownView.swift
@@ -5,4 +5,16 @@
 //  Created by Azella Mutyara on 24/08/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct CrownView: View {
+    var body: some View {
+        Text("Crown")
+    }
+}
+
+struct CrownView_Previews: PreviewProvider {
+    static var previews: some View {
+        CrownView()
+    }
+}
